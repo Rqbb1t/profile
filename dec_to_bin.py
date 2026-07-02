278780d570659b1c5f66523e86f350b298870b92
Rqbb1t <charu100825@gmail.com>
2026-07-02 11:47:00 +0700

    Add files via upload

---
+++ dec_to_bin.py
@@ -0,0 +1,8 @@
+dec = int(input("Enter a decimal number: ")) #เลขฐาน 10
+conv_to1 = bin(dec) #แปลงเป็นเลขฐาน 2
+conv_to2 = oct(dec) #แปลงเป็นเลขฐาน 8
+conv_to3 = hex(dec) #แปลงเป็นเลขฐาน 16
+print("Binary Number : ",conv_to1) #แสดงผลลัพธ์
+print("Octal Number : ",conv_to2) #แสดงผลลัพธ์
+print("Hexadecimal Number : ",conv_to3) #แสดงผลลัพธ์
+print("สร้างโดย : นายธรรมนูญ ศรีพนมวรรณ รหัส 694245006")
